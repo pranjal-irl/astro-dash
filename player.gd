0e6fcdc4c9d04fc8bb9cedd462f02740007ae7f4
Pranjal <pranjal.quantom@gmail.com>
2026-06-30 13:14:17 +0530

    restricted player movement area and added realistic asteroid

--- player.gd
+++ player.gd
@@ -6,3 +6,6 @@ func _physics_process(delta):
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = direction * speed
 	move_and_slide()
+	
+	position.x = clamp(position.x, 0, 1152)
+	position.y = clamp(position.y, 324, 648)
